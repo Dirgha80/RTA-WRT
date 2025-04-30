62d411ecf47b6f5e805ddf8552273424e6400a6f
Dirgha80 <156698511+Dirgha80@users.noreply.github.com>
2025-04-30 09:44:38 +0700

    Update REPACKWRT.sh

--- scripts/REPACKWRT.sh
+++ scripts/REPACKWRT.sh
@@ -56,7 +56,7 @@ repackwrt() {
     fi
 
     # Define constants
-    local readonly OPHUB_REPO="https://github.com/Dirgha/amlogic-s9xxx-openwrt/archive/refs/heads/main.zip"
+    local readonly OPHUB_REPO="https://github.com/Dirgha80/amlogic-s9xxx-openwrt/archive/refs/heads/main.zip"
     local readonly ULO_REPO="https://github.com/armarchindo/ULO-Builder/archive/refs/heads/main.zip"
     local readonly work_dir="$GITHUB_WORKSPACE/$WORKING_DIR"
     
@@ -132,7 +132,7 @@ repackwrt() {
     local device_output_dir
     if [[ "$builder_type" == "--OPHUB" ]] || [[ "$builder_type" == "--ophub" ]]; then
         log "INFO" "Running OphubBuilder..."
-        if ! sudo ./remake -b "${target_board}" -k "${target_kernel}" -s 1024; then
+        if ! sudo ./remake -b "${target_board}" -k "${target_kernel}" -s ${{ inputs.rootfs_size }}; then
             error_msg "OphubBuilder execution failed"
             exit 1
         fi
